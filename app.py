3fac66c62203dfbb55583c10030902f2d6f50f94
Ashford <ashfordrebelo147@gmail.com>
2024-12-03 18:51:22 +0530

    dog_model.py file

--- app.py
+++ app.py
@@ -251,4 +251,4 @@ if __name__ == '__main__':
     # Ensure uploads folder exists
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5555)
