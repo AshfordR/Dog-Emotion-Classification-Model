95c6946410e29ed42d561d02646c241ea170b11c
Ashford <ashfordrebelo147@gmail.com>
2024-12-06 12:29:06 +0530

    final

--- app.py
+++ app.py
@@ -463,4 +463,4 @@ if __name__ == '__main__':
     # Ensure uploads folder exists
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(debug=True, host='0.0.0.0', port=5003)
+    app.run(debug=True, host='0.0.0.0', port=5000)
